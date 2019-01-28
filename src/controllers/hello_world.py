71b33d87839b3060d1f4c638bc7eb0c8cfeaa876
CassioRoos <cassio.roos@softplan.com.br>
2019-01-28 14:49:46 -0200

    ajuste projeto

--- src/controllers/hello_world.py
+++ src/controllers/hello_world.py
@@ -1,6 +1,7 @@
 import time
 from http import HTTPStatus
 from random import randint
+from src.configurations import config
 
 from src.controllers.base import BaseHandler
 
@@ -8,4 +9,4 @@ from src.controllers.base import BaseHandler
 class HelloWorld(BaseHandler):
     async def get(self):
         time.sleep(randint(1, 7))
-        return self.writeResponse(HTTPStatus.OK, "HELLO WORLD!")
+        return self.writeResponse(HTTPStatus.OK, f"HELLO WORLD! From APP {config.APP_ID}")
